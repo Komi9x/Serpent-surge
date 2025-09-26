dd355a14a2a77b52ab38dd90f8aae7429b5b656a
Steven Komlossy <komi9x@gmail.com>
2025-09-26 17:04:24 +0200

    Delete serpent-surge-main directory
    because of the bad structure.

--- serpent-surge-main/backend/index.js
+++
@@ -1,8 +0,0 @@
-// Database credentials - environment variables
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'localhost',
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'MeMyselfandI_1928',
-    database: process.env.MYSQL_DATABASE || 'snake_game',
-    port: '3306',
-});
